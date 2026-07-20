06636ecbbb0cea5cf31c1da66ffdcd842bc323c1
zeynepaytekin <152427296+zeynepaytekin@users.noreply.github.com>
2026-07-20 14:45:46 +0300

    github pages settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const repoName = "aksiyoncuk";
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isProduction ? `/${repoName}` : "",
+  assetPrefix: isProduction ? `/${repoName}/` : "",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
